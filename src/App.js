6e1a82d462fb710f4b3ce58d5a7c25e6dd9b24c2
Chandan Kasamsetty <34879929+chandan2300@users.noreply.github.com>
2022-03-27 20:17:33 +0530

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
 // import Signup from "./sing-up";
 import Register from "./Register";
+import ResponsiveAppBar from "./Navbar";
 
 function App() {
   return (
     <div className="App">
-      <Register />
+      <ResponsiveAppBar />
     </div>
   );
 }
